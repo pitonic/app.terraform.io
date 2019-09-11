b137f1631477b83d53a628e95859dc11317a955c
pitonic <pitonic@users.noreply.github.com>
2019-09-11 00:37:11 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+#
+# Using backend  app.terraform.io
+#
+
+terraform {
+  backend "remote" {
+    organization = "PITONic"
+    workspaces {
+      name = "clouds_piton"
+    }
+  }
+}
+
+
+provider "aws" {
+ region  = "us-east-1"
+}
+
+resource "aws_instance" "my_ubuntu" {
+ 
+ ami = "ami-0b898040803850657"
+ instance_type = "t2.micro"
+  tags = {
+    Name = "super att"
+    Name2 = "ec2"
+    }
+ 
+}
+       
