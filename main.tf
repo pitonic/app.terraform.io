634a8cf433518f77cfc0a8443d7d9a1d037fdbf0
pitonic <pitonic@users.noreply.github.com>
2019-09-11 00:43:05 -0400

    fix typo
    go Ahead

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "my_ubuntu" {
     Name2 = "ec2"
     }
  
-  resource "aws_instance" "my_ubuntu_2" {
+resource "aws_instance" "my_ubuntu_2" {
  
  ami = "ami-0b898040803850657"
  instance_type = "t2.micro"
@@ -35,7 +35,7 @@ resource "aws_instance" "my_ubuntu" {
     Name2 = "ec2_t2.micro"
     }
   
-  
+  }
   
 }
        
