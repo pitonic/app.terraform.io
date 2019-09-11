5243ce6ab02be234493889229a50e852b05d7fab
pitonic <pitonic@users.noreply.github.com>
2019-09-11 16:30:57 -0400

    Update main.tf

--- project1/main.tf
+++ project1/main.tf
@@ -28,13 +28,4 @@ resource "aws_instance" "my_ubuntu" {
  
 }
 
-resource "aws_instance" "my_ubuntu2" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "Project1-server2"
-    Name2 = "ec2-2"
-    }
- 
-}
+
