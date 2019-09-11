403f19907ce7ecf6a252e3cef277fe2dce86dd82
pitonic <pitonic@users.noreply.github.com>
2019-09-11 00:51:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,24 +15,4 @@ terraform {
 
 provider "aws" {
  region  = "us-east-1"
-}
-
-resource "aws_instance" "my_ubuntu" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "super att"
-    Name2 = "ec2"
-    }
-  }
-resource "aws_instance" "my_ubuntu_2" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "my_ubuntu_2"
-    Name2 = "ec2_t2.micro"
-    }
-}
-       
+} 
