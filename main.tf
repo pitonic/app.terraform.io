0780548ea615c990d9309e5fb110565db893aed2
pitonic <pitonic@users.noreply.github.com>
2019-09-11 09:38:52 -0400

    fff

--- main.tf
+++ main.tf
@@ -17,13 +17,3 @@ provider "aws" {
  region  = "us-east-1"
 } 
 
-resource "aws_instance" "my_ubuntu" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "super att"
-    Name2 = "ec2"
-    }
- 
-}
