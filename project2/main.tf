ea6fe6dde049ceb9e4072aa944d66d4718b8c0b2
pitonic <pitonic@users.noreply.github.com>
2019-09-11 16:15:29 -0400

    22222

--- project2/main.tf
+++ project2/main.tf
@@ -29,26 +29,3 @@ resource "aws_instance" "my_ubuntu" {
 }
 
 
-resource "aws_instance" "my_ubuntu-www" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "SERVER-2 Project -2 "
-    Name2 = "ec2-2"
-    }
- 
-}
-
-
-resource "aws_instance" "my_ubuntu-3" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "SERVER-3 Project -2 "
-    Name2 = "ec2-33332"
-    }
- 
-}
-
