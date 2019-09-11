fdbbf1cb306243117a3b67320e2a41bbbe1721de
pitonic <pitonic@users.noreply.github.com>
2019-09-11 00:41:03 -0400

    second instance
    run forest run

--- main.tf
+++ main.tf
@@ -26,5 +26,16 @@ resource "aws_instance" "my_ubuntu" {
     Name2 = "ec2"
     }
  
+  resource "aws_instance" "my_ubuntu_2" {
+ 
+ ami = "ami-0b898040803850657"
+ instance_type = "t2.micro"
+  tags = {
+    Name = "my_ubuntu_2"
+    Name2 = "ec2_t2.micro"
+    }
+  
+  
+  
 }
        
