abcab254cc5118d342ae40e0621374905e163c4a
pitonic <pitonic@users.noreply.github.com>
2019-09-11 17:41:59 -0400

    Update main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "my_ubuntu" {
  # Please check "AMI"  different on each region    
  ami = "ami-0b898040803850657"
  instance_type = "t2.micro"
- # If default VPC deleted, please define -> subnet_id ="subnet-123aaa"
- #   
- # subnet_id     = "subnet-01ec8cd3246f04afa"
- 
+
+   tags {
+    name = "project-2"
+    }
 }
