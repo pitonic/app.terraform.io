ad70fdcd9b4f1a580fd7ce036695c2638bb7f605
pitonic <pitonic@users.noreply.github.com>
2019-09-11 16:35:59 -0400

    Name = "WebServer_p2"

--- project2/main.tf
+++ project2/main.tf
@@ -1,20 +1,15 @@
 
-#
-# Using backend  app.terraform.io
-#
-
-/*terraform {
-  backend "remote" {
-    organization = "PITONic"
-    workspaces {
-      name = "project2"
-    }
-  }
-}
-*/
 
 provider "aws" {
  region  = "us-east-1"
 }
 
-
+resource "aws_instance" "WebServer" {
+ 
+ ami = "ami-0b898040803850657"
+ instance_type = "t2.micro"
+  tags = {
+    Name = "WebServer_p2"
+    }
+ 
+}
