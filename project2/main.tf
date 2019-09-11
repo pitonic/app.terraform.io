d72e8aa4707551dd18b840bd3c396e5f9a371c0a
pitonic <pitonic@users.noreply.github.com>
2019-09-11 17:06:10 -0400

    ppp22222

--- project2/main.tf
+++ project2/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region                  = "us-east-1"
  }
+
+
+
+resource "aws_instance" "my_ubuntu" {
+ # Please check "AMI"  different on each region    
+ ami = "ami-0b898040803850657"
+ instance_type = "t2.micro"
+ # If default VPC deleted, please define -> subnet_id ="subnet-123aaa"
+ #   
+ # subnet_id     = "subnet-01ec8cd3246f04afa"
+ 
+}
