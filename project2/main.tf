dcaabee3bf754faa065780cee2d8c885dd1e37ec
pitonic <pitonic@users.noreply.github.com>
2019-09-11 16:27:57 -0400

    Update main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -17,15 +17,4 @@ provider "aws" {
  region  = "us-east-1"
 }
 
-resource "aws_instance" "my_ubuntu" {
- 
- ami = "ami-0b898040803850657"
- instance_type = "t2.micro"
-  tags = {
-    Name = "Project -2 "
-    Name2 = "ec2-2"
-    }
- 
-}
-
 
